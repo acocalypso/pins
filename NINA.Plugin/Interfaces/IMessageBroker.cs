f4b792aed8fdea60eba6b0abe5c6001124f7fe3c
Nico <nitro.mail@web.de>
2026-07-24 12:17:27 +0200

    cancelable Publish

--- NINA.Plugin/Interfaces/IMessageBroker.cs
+++ NINA.Plugin/Interfaces/IMessageBroker.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NINA.Plugin.Interfaces {
     public interface IMessageBroker {
         Task Publish(IMessage message);
+        Task Publish(IMessage message, CancellationToken token);
         void Subscribe(string topic, ISubscriber subscriber);
         void Unsubscribe(string topic, ISubscriber subscriber);
     }
